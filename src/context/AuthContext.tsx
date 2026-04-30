603daeb6d02422276b86f6b89580124500acf5df
haaaaazahid <shaikhmohdzahid992@gmail.com>
2026-04-30 22:42:39 +0530

    fix auth flow and remove conflicts

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -77,14 +77,13 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     return !error;
   }, []);
 
-  const signInWithGoogle = useCallback(async () => {
-    return await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: "/https://jcsam-hub.vercel.app//login",
-      },
-    });
-  }, []);
+const signInWithGoogle = useCallback(async () => {
+  return await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: window.location.origin + "/admin",    },
+  });
+}, []);
 
   return (
     <AuthContext.Provider value={{
